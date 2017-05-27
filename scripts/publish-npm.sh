15cb9342cf7057c1fad9f0e767b6d3bcbb8174f9
erichua(华挺) <erichua@webank.com>
2017-05-27 20:13:59 +0800

    0.1.5

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,4 +1,14 @@
-npm publish --access public --verbose
+#!/bin/bash
+
+# if [ $# -lt 1 ];then
+# echo 'Your should input your version numver'
+# exit
+# fi
+
 git add -A
 git commit
-git push
+
+version=v$1
+git tag version
+npm publish --access public --tag $version --verbose
+git push --tag
