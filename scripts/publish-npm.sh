5c7f8632667b5b78d74fbc2f4d406025401c1a9a
EricHua23 <723943634@qq.com>
2017-08-06 13:33:37 +0800

    Update publish-npm.sh

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# if [ $# -lt 1 ];then
-# echo 'Your should input your version numver'
-# exit
-# fi
-
 git add -A
 git commit
 
-version=v$1
-git tag version
+version=`npm version patch`
 npm publish --access public --verbose
+git push 
 git push --tag
