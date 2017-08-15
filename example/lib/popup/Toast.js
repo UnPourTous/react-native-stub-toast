015db6f6f8e69b7e384ec87f05b74a4c6a1b261c
erichua <erichua@erichuas-MacMini.local>
2017-08-15 18:02:24 +0800

    support custom toast

--- example/lib/popup/Toast.js
+++ example/lib/popup/Toast.js
@@ -10,6 +10,11 @@ import React, { Component } from 'react'
 
 export default class Toast extends Component {
   static _popupStub
+  static _refs = {}
+  /**
+   * @type {Array}
+   * @private
+   */
   static _toastQueue = []
   static _isToastShowing = false
 
@@ -17,10 +22,12 @@ export default class Toast extends Component {
     Toast._popupStub = popupStub
   }
 
-  static show (msg, duration) {
+  static show (msg, duration, durationIn, durationOut) {
     const toast = {
       msg,
-      duration
+      duration,
+      durationIn,
+      durationOut
     }
     if (Toast._isToastShowing) {
       Toast._toastQueue.push(toast)
@@ -29,24 +36,64 @@ export default class Toast extends Component {
       Toast._showToastQueue()
     }
   }
+
+  /**
+   * @param toastConfig
+   * {
+   *   msg: '', // for simple toast with only text
+   *   duration: '', // show duration
+   *   component: '' // for custom toast with custom component,
+   *   durationIn: '' // duration for in animation
+   *   durationOut: '' // duration for out animation
+   * }
+   */
+  static showCustom (toastConfig) {
+    if (Toast._isToastShowing) {
+      Toast._toastQueue.push(toastConfig)
+    } else {
+      Toast._toastQueue.push(toastConfig)
+      Toast._showToastQueue()
+    }
+  }
+
   static _showToastQueue () {
     const toast = Toast._toastQueue.shift()
     Toast._isToastShowing = true
     const id = Toast._popupStub.getNewId()
-    Toast._popupStub.addPopup(
-      <Animatable.View animation='fadeIn' style={styles.content}>
-        <Text style={styles.text}>{toast.msg}</Text>
-      </Animatable.View>,
-      id
-    )
+    if (toast.component) {
+      Toast._popupStub.addPopup(
+        <Animatable.View
+          ref={(_ref) => {
+            if (_ref) {
+              Toast._refs[id] = _ref
+            }
+          }}
+          animation='fadeIn'
+          duration={toast.durationIn || 300}>
+          {toast.component}
+        </Animatable.View>,
+        id
+      )
+    } else {
+      Toast._popupStub.addPopup(
+        <Animatable.View animation='fadeIn' duration={toast.durationIn || 300} style={styles.content}>
+          <Text style={styles.text}>{toast.msg}</Text>
+        </Animatable.View>,
+        id
+      )
+    }
+
 
     setTimeout(() => {
-      Toast._popupStub.removePopup(id)
-      if (Toast._toastQueue.length > 0) {
-        Toast._showToastQueue()
-      } else {
-        Toast._isToastShowing = false
-      }
+      Toast._refs[id]
+      Toast._refs[id] && Toast._refs[id].fadeOut && Toast._refs[id].fadeOut(toast.durationOut || 300).then(() => {
+        Toast._popupStub.removePopup(id)
+        if (Toast._toastQueue.length > 0) {
+          Toast._showToastQueue()
+        } else {
+          Toast._isToastShowing = false
+        }
+      })
     }, toast.duration || 1000)
   }
 }
